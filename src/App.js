698cd2e1570816b4d1bc048b7f27a532cd2b3116
mehul9595 <mehul9595@gmail.com>
2021-01-13 16:26:22 +0800

    create header component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import logo from "./logo.svg";
-import React, { Image } from "react";
+import React  from "react";
+import Header from './components/Header';
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="title">
-          <div className="first-line">Scoober - Takeaway.com</div>
-          <div className="sub-title">Win the game or win the job</div>
-        </div>
-      </header>
+      <Header />
       <p>This is paragraph</p>
     </div>
   );
