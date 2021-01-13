6d851697249b9c4f8cebccceb415ff5ff8bf38c4
mehul9595 <mehul9595@gmail.com>
2021-01-13 16:53:47 +0800

    creating home screen board

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React  from "react";
-import Header from './components/Header';
+import React from "react";
+import Header from "./components/Header";
+import Layout from "./containers/Layout";
+import Home from './components/Home'
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <p>This is paragraph</p>
+      <Layout>
+        <Header />
+        <Home />
+        <p>This is paragraph</p>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';  
+
+const Home = (props) => {
+    return (
+        <>
+            <div className="home">
+                <h1>Game of Three</h1>
+                <p>Single Player</p>
+                <p>Multiplayer</p>
+            </div>
+        </>
+    );
+}
+
+export default Home;
