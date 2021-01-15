41385a0d46c02245c3e050adc8e5f05f412a059f
mehul9595 <mehul9595@gmail.com>
2021-01-15 12:29:23 +0800

    adding initial logic for turn counter and log message

--- src/containers/SinglePlayer/index.js
+++ src/containers/SinglePlayer/index.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "../../components/Header";
 import PlayArea from "../../components/PlayArea";
 import ActionButton from "../../components/ActionButton";
+import { message } from 'antd';
+import "antd/dist/antd.css";
 
 const SinglePlayer = (props) => {
+  const [turnCount, setTurnCount] = useState(0);
+  const [turnArray, setTurnArray] = useState([{
+    id: 0,
+    player: "Bot",
+    value: 50
+  }]);
+
+  const checkActionAndResult = (params) => {
+    message.info(params);
+    var turnCounter = turnCount + 1;
+    setTurnCount(turnCounter);
+
+    var newTurn = {
+      id: turnCounter,
+      player: "player",
+      value: Math.random()
+    }
+    setTurnArray(result=> [...result, newTurn]);
+    console.log(turnArray);
+  }
+  
+
   return (
     <>
       <Header />
-      <PlayArea />
-      <ActionButton />
+      <PlayArea turnArray={turnArray} />
+      <ActionButton actionHandler={checkActionAndResult} />
     </>
   );
 };
