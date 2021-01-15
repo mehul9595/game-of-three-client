bea8759133ba867cd44a0e1abe1f8e92be0e3722
mehul9595 <mehul9595@gmail.com>
2021-01-15 12:27:58 +0800

    adding antd list to display actions

--- src/components/PlayArea/index.js
+++ src/components/PlayArea/index.js
@@ -1,20 +1,19 @@
 import React from "react";
+import { List } from "antd";
 import "./style.css";
 
 const PlayArea = (props) => {
   return (
     <>
-      <div className="play-area-container">
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </div>
+      <List
+        dataSource={props.turnArray}
+        renderItem={(turn) => (
+          <List.Item key={turn.id} className={"playarea-number " + turn.player}>
+            <div>{turn.player === "player" ? "You" : "Bot"}:</div>
+            <div>{turn.value}</div>
+          </List.Item>
+        )}
+      />
     </>
   );
 };
