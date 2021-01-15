c1026b96d9607590b1894f6995ce4ea7e883eed4
mehul9595 <mehul9595@gmail.com>
2021-01-15 12:49:51 +0800

    changing list component to use avatar

--- src/components/PlayArea/index.js
+++ src/components/PlayArea/index.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { List } from "antd";
+import { List, Avatar } from "antd";
 import "./style.css";
 
 const PlayArea = (props) => {
   return (
     <>
-      <List
+      <List className="play-area-container"
         dataSource={props.turnArray}
         renderItem={(turn) => (
           <List.Item key={turn.id} className={"playarea-number " + turn.player}>
-            <div>{turn.player === "player" ? "You" : "Bot"}:</div>
-            <div>{turn.value}</div>
+            <List.Item.Meta
+              avatar={
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              }
+              title={turn.player === "player" ? "You" : "Bot"}
+              description={turn.player.value}
+            ></List.Item.Meta>
           </List.Item>
         )}
       />
